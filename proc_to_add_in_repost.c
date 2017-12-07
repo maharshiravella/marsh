3226d398e23bdddd13375f6520c0113260333f7a
maharshiravella <maharshiravella97@gmail.com>
2017-12-07 11:02:24 +0530

    second commit

---
+++ proc_to_add_in_repost.c
@@ -0,0 +1,7 @@
+1.git init Mytest
+ create files in mytest directory
+2.git add README
+3.git commit -m "some_message"
+4.git remote add origin https://github.com/user_name/Mytest.git
+5.git push origin master
+
